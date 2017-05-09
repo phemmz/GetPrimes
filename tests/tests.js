c3e5c1313e3dfe23a1e2da8ffd7dc0fe524585eb
Adetunji Femi <phemmzmcllroy@gmail.com>
2017-05-09 19:45:39 +0100

    test cases update

--- tests/tests.js
+++ tests/tests.js
@@ -7,34 +7,36 @@ const myApp = require('../app/library.js');
 
 describe("Arithmetic Functionality", function() {
   it("should return an empty array if no argument is supplied", function() {
-  	assert(myApp.getPrime() == []);
+	  expect(getPrime()).toEqual([]);  
   })
   it("should return an empty array if 0 is supplied as argument", function() {
-  	assert(myApp.getPrime(0) == []);
+	  expect(getPrime(0)).toEqual([]);
   })
   it("should return invalid input message if -5 is supplied as argument", function() {
-  	assert(myApp.getPrime(-5) == 'invalid input');
+	  expect(getPrime(-5)).toEqual('invalid input');
   })
-  it("should return invalid input message if -25 is supplied as argument", function() {
-  	assert(myApp.getPrime(-25) == 'invalid input');
+  it("should throw an error", function() {
+	  let throMeAnError = () => {
+		  throw new Error();
+	  }expect(throwMeAnError).toThrow();
   })
-  it("should return invalid input message if -30 is supplied as argument", function() {
-  	assert(myApp.getPrime(-30) == 'invalid input');
+  it("should be true if it evaluates to true", function() {
+	  expect(getPrime(30)).toBeTruthy();
   })
-  it("should return invalid input message if type of argument supplied is a string", function() {
-  	assert(myApp.getPrime('boy') == 'invalid input');
-  })
-  it("should return a list of all prime numbers from 0 to the number of the argument supplied", function() {
-  	assert(myApp.getPrime(3) == [2, 3]);
-  })
-  it("should return a list of all prime numbers from 0 to the number of the argument supplied", function() {
-  	assert(myApp.getPrime(10) == [2, 3, 5, 7]);
+  it("should be a number", function() {
+	  expect(getPrime(3)).not.toBeNaN();
   })
   it("should return a list of all prime numbers from 0 to the number of the argument supplied", function() {
-  	assert(myApp.getPrime(30) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+	  expect(getPrime(3)).toEqual([2,3]);
   })
-  it("should return an invalid input message if an empty string argument is supplied", function() {
-  	assert(myApp.getPrime("") == 'invalid input');
+  it("should return a list of all prime numbers from 0 to the number of the argument supplied", function() {
+	  expect(getPrime(10)).toEqual([2, 3, 5, 7]);
+  })
+  it("should return a list of all prime numbers from 0 to the number of the argument supplied", function() {
+	  expect(getPrime(30)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+  })
+  it("should contain an element of the array returned", function() {
+	  expect(getPrime(30)).toContain(5);
   })
 })
 
